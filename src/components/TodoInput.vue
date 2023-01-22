32a2b426e3ab4d9695213b2029fe9a9b3369a0fb
jeonghee <grighth12@gmail.com>
2023-01-23 03:09:50 +0900

    feat: todo input 제출한 후 빈 값으로 변환

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,6 +1,6 @@
 <template>
-  <form @submit.prevent="$emit('submitTodo', value)">
-    <input type="text" v-model="value" ref="inputRef" />
+  <form @submit.prevent="submitTodo">
+    <input type="text" v-model="todo" ref="inputRef" />
     <input type="submit" />
   </form>
 </template>
@@ -11,8 +11,8 @@ import { ref, watch } from "vue";
 export default {
   // * emit 명이 native event명과 같은 경우 문제 발생 sumbit(event), submit(value)
   emit: ["submitTodo"],
-  setup() {
-    const value = ref("");
+  setup(props, { emit }) {
+    const todo = ref("");
     const inputRef = ref();
 
     watch(inputRef, () => {
@@ -21,9 +21,15 @@ export default {
       }
     });
 
+    const submitTodo = () => {
+      emit("submitTodo", todo.value);
+      todo.value = "";
+    };
+
     return {
-      value,
+      todo,
       inputRef,
+      submitTodo,
     };
   },
 };
