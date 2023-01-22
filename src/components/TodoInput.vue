c0baa5571c863469ea4908c658318e8b5b580494
jeonghee <grighth12@gmail.com>
2023-01-23 02:56:17 +0900

    feat: todo input 제출

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent="$emit('submitTodo', value)">
+    <input type="text" v-model="value" />
+    <input type="submit" />
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  emit: ["submitTodo"],
+  setup() {
+    const value = ref("");
+
+    return {
+      value,
+    };
+  },
+};
+</script>
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,10 +1,37 @@
 <template>
   <h1 class="page-title">할 일 목록</h1>
+  <TodoInput @submitTodo="handleSubmitTodo" />
+  <div>{{ todos }}</div>
 </template>
+
+<script>
+import TodoInput from "../components/TodoInput.vue";
+import { reactive } from "vue";
+
+export default {
+  // * export default를 이용할 경우에 components를 등록해주어야 한다
+  components: {
+    TodoInput,
+  },
+  setup() {
+    const todos = reactive([]);
+
+    const handleSubmitTodo = (todo) => {
+      todos.push(todo);
+    };
+
+    return {
+      todos,
+      handleSubmitTodo,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .page-title {
   font-size: 40px;
   font-weight: 700;
+  margin-bottom: 16px;
 }
 </style>
