10e66028b638b22279499130302d4f988bed9626
jeonghee <grighth12@gmail.com>
2023-01-23 03:36:36 +0900

    feat: todo item

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="todo-item">{{ todo }}</li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: String,
+  },
+  setup() {},
+};
+</script>
+
+<style scoped>
+.todo-item {
+  padding: 5px 20px;
+}
+</style>
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,17 +1,22 @@
 <template>
   <h1 class="page-title">할 일 목록</h1>
   <TodoInput @submitTodo="handleSubmitTodo" />
-  <div>{{ todos }}</div>
+
+  <ul>
+    <TodoItem v-for="todo in todos" :key="todo" :todo="todo" />
+  </ul>
 </template>
 
 <script>
 import TodoInput from "../components/TodoInput.vue";
+import TodoItem from "../components/TodoItem.vue";
 import { reactive } from "vue";
 
 export default {
   // * export default를 이용할 경우에 components를 등록해주어야 한다
   components: {
     TodoInput,
+    TodoItem,
   },
   setup() {
     const todos = reactive([]);
@@ -33,5 +38,9 @@ export default {
   font-size: 40px;
   font-weight: 700;
   margin-bottom: 16px;
+}
+
+ul {
+  list-style: inside;
 }
 </style>
